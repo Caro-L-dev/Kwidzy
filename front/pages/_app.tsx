7e29fbdac39b6ca5855c11492ff411bba6eb253f
caroline <liardcar@gmail.com>
2023-03-10 10:50:40 +0100

    style(app): import font

--- front/pages/_app.tsx
+++ front/pages/_app.tsx
@@ -1,11 +1,21 @@
-import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Container from "@/components/molecules/Container/Container";
+
+import "@/styles/globals.css";
+import { Rammetto_One } from "@next/font/google";
+
+// https://nextjs.org/docs/api-reference/next/font
+const mainFont = Rammetto_One({
+  subsets: ["latin"],
+  weight: "400",
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Container>
-      <Component {...pageProps} />
+      <main className={mainFont.className}>
+        <Component {...pageProps} />
+      </main>
     </Container>
   );
 }
--- front/pages/index.tsx
+++ front/pages/index.tsx
@@ -3,8 +3,8 @@ import MainBtn from "@/components/atoms/Button/MainBtn";
 export default function Home() {
   return (
     <>
-      <div className="pt-80 md:h-screen">
-        <h1 className="font-bold text-3xl md:text-8xl mb-20">KWIDZY</h1>
+      <div className="pt-80 h-full">
+        <h1 className="font-bold text-3xl md:text-8xl mb-8">KWIDZY</h1>
         <div>
           <MainBtn>
             <a className="font-bold text-3xl" href={"/start"}>
