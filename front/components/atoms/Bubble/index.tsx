a32e4a95ae885eb53dd3fd05d4199b91affc87be
caroline <liardcar@gmail.com>
2023-03-13 09:27:03 +0100

    feat(components/triangle): create triangle shape

--- front/components/atoms/Bubble/index.tsx
+++ front/components/atoms/Bubble/index.tsx
@@ -3,15 +3,23 @@
  */
 import React from "react";
 
+/**
+ * local Import
+ */
+import { Triangle } from "../index";
+
 interface BubbleProps {
   children?: React.ReactNode;
 }
 
+// Corriger position Triangle
 const Bubble: React.FC<BubbleProps> = ({ children }) => {
   return (
     <div className="relative my-6">
-      {/* Triangle Shape */}
-      <div className="absolute right-0 top-0 w-0 h-0 border-l-[30px] border-l-transparent border-r-[30px] border-r-transparent border-b-[50px] border-b-tertiary-color" />
+      <Triangle
+        className={"absolute right-[-2px] top-0"}
+        position={"side-bubble"}
+      />
       <p className="p-4 bg-tertiary-color rounded-full mb-6 tracking-wide text-sm z-30">
         {children}
       </p>
