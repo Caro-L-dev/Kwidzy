a51d6a257592b954d9ffc2e209e91924e418c89d
caroline <liardcar@gmail.com>
2023-04-11 23:16:23 +0200

    chore: retrieve questions and answer datas

--- front/src/components/molecules/Quiz/Quiz.tsx
+++ front/src/components/molecules/Quiz/Quiz.tsx
@@ -1,7 +1,8 @@
 /**
  * Package Import
  */
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
+import axios from "axios";
 
 /**
  * Local Import
@@ -12,9 +13,17 @@ import { Button } from "@/src/components/atoms";
  * Types
  */
 interface QuestionState {
+  id: number;
+  letter: string;
   questionNumber: number;
   question: string;
   answers: answerState[];
+}
+
+interface QuestionStateApi {
+  id: number;
+  category_id: number;
+  question_text: string;
 }
 
 interface answerState {
@@ -22,6 +31,19 @@ interface answerState {
   text: string;
   correct: boolean;
 }
+
+interface answerStateApi {
+  id: number;
+  question_id: number;
+  answer_text: string;
+  is_correct: boolean;
+}
+
+/**
+ * Datas
+ */
+const questionURL = "http://localhost:3030/question";
+const answerURL = "http://localhost:3030/answer";
 
 /**
  * Component
@@ -42,15 +64,59 @@ export default function QuizMolecule({
    */
   const [question, setQuestion] = useState<QuestionState | null>(null);
   const [selectedAnswer, setSelectedAnswer] = useState<string | null>(null);
+
+  const [questionApi, setQuestionApi] = useState<QuestionStateApi[] | null>(
+    null
+  );
+  const [answerApi, setAnswerApi] = useState<answerStateApi[] | null>(null);
+
   const [className, setClassName] = useState("");
   const [variant, setVariant] = useState("primary");
+
+  const quizData2: QuestionState[] = useMemo(() => {
+    if (!questionApi || !answerApi) {
+      return [];
+    }
+    return questionApi.map((questionObject) => {
+      return {
+        id: questionObject.id,
+        questionNumber: `Q${questionObject.id}. `,
+        question: questionObject.question_text,
+        answers: answerApi
+          .filter((answerObject) => {
+            return answerObject.question_id === questionObject.id;
+          })
+          .map((answerObject) => {
+            return {
+              text: answerObject.answer_text,
+              correct: answerObject.is_correct,
+            };
+          }),
+      };
+    });
+  }, [questionApi, answerApi]);
 
   /**
    * Lifecycle
    */
   useEffect(() => {
-    setQuestion(quizData[questionNumber - 1]);
-  }, [quizData, questionNumber]);
+    setQuestion(quizData2[questionNumber - 1]);
+  }, [quizData2, questionNumber]);
+
+  /**
+   * Fetch datas
+   */
+  React.useEffect(() => {
+    axios.get(questionURL).then((response) => {
+      setQuestionApi(response.data);
+    });
+  }, []);
+
+  React.useEffect(() => {
+    axios.get(answerURL).then((response) => {
+      setAnswerApi(response.data);
+    });
+  }, []);
 
   /**
    * Delay Function
@@ -100,6 +166,7 @@ export default function QuizMolecule({
             type={"button"}
             variant={selectedAnswer === answer.text ? variant : "primary"}
             rounded
+            className={""}
           >
             <span className="mr-4">{answer.letter}</span>
             <p>{answer.text}</p>
